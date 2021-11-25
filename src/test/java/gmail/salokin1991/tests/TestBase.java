0886918a4813c74dae0abfbb342615ecce7cc82a
Hermit91 <work.salokin@gmail.com>
2021-11-25 17:22:28 +0300

    fix remote address

--- src/test/java/gmail/salokin1991/tests/TestBase.java
+++ src/test/java/gmail/salokin1991/tests/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
     static void beforeAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotest.cloud/wd/hub/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
