1549a2c20b2760bb75114ba57b1bfee560b712a1
Hermit91 <work.salokin@gmail.com>
2021-11-25 17:02:08 +0300

    Added attacments

--- src/test/java/gmail/salokin1991/tests/TestBase.java
+++ src/test/java/gmail/salokin1991/tests/TestBase.java
@@ -2,10 +2,13 @@ package gmail.salokin1991.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import gmail.salokin1991.helpers.Attach;
 import gmail.salokin1991.pages.RegistrationPage;
 import gmail.salokin1991.utils.FakeDataUtil2;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -16,5 +19,20 @@ public class TestBase {
     static void beforeAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotest.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
